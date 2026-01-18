9b0c45325d5c88b57090bf4cb3e9e7f3171b0cfa
Tharindu Dhanushka <thdhanushka31@gmail.com>
2026-01-18 11:16:02 +0530

    Update README.md

---
+++ multiple lines.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Tharindu Dhanushka"<<endl;
+    cout<<"I'm 22 years old. \n";
+    cout<<"I am from Kegalle.";
+    return 0;
+
+}
